b8d8db83c70c0afa8841afd83e18ac24c89ab45a
09Demon <34493949+09DZL@users.noreply.github.com>
2023-12-03 19:59:04 -0500

    Palatte Application

---
+++ PalleteApplication/src/SampleImage.py
@@ -0,0 +1,16 @@
+from tkinter import PhotoImage
+
+class SampleImage():
+    
+    def __main__(self, photo:PhotoImage):
+        self.image = photo
+
+    def transform_color():
+        print("transform color")
+
+    def pack(x, y):
+        print("Do something")
+
+
+
+
